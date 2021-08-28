0d2442d12fd4e6483f54d2df04ec62f2b9346eb3
andresgmejiar <46786423+andresgmejiar@users.noreply.github.com>
2021-08-28 04:44:53 -0400

    Add files via upload

--- install.R
+++ install.R
@@ -1,7 +1,5 @@
-list.of.packages <- c("ggplot2","plotly","data.table","stringr","stringi","cluster","wordcloud","R.utils","apt-utils")
+list.of.packages <- c("ggplot2","plotly","data.table","stringr","stringi","cluster","wordcloud","R.utils")
 install.packages(list.of.packages)
-
-gdata::installXLSXsupport()
 
 library(stringr)
 library(data.table)
