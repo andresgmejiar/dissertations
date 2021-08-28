2d3e19005f86d78f241f194301d06df41349faa3
andresgmejiar <46786423+andresgmejiar@users.noreply.github.com>
2021-08-28 04:04:15 -0400

    Add files via upload

---
+++ install.R
@@ -0,0 +1,9 @@
+list.of.packages <- c("ggplot2","plotly","data.table","stringr","stringi","cluster","wordcloud")
+install.packages(list.of.packages)
+
+gdata::installXLSXsupport()
+
+library(gdata) # for the trim function
+library(stringr)
+library(data.table)
+
